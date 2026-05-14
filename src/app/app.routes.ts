c4e2ac7a4665d87ba2084075faffc2ba194aae81
insanetim <insanetim1986@gmail.com>
2026-05-14 14:17:04 +0300

    feat: base layout and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PlacePage } from './pages/place-page/place-page';
+import { SearchPage } from './pages/search-page/search-page';
+import { WishlistPage } from './pages/wishlist-page/wishlist-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'search', pathMatch: 'full' },
+  { path: 'search', component: SearchPage },
+  { path: 'wishlist', component: WishlistPage },
+  { path: 'place/:id', component: PlacePage },
+  { path: '**', redirectTo: 'search' },
+];
---
+++ src/app/pages/place-page/place-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-place-page',
+  imports: [],
+  templateUrl: './place-page.html',
+  styleUrl: './place-page.scss',
+})
+export class PlacePage {}
